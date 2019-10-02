8ad3b74fa6c9c3210ecd82a6397dea4663570f31
Julie Cortez <jlcortez@madisoncollege.edu>
2019-10-02 15:01:41 -0500

    update data to reset data

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,2 +1,6 @@
-delete from Role;
-insert into Role values (1, 'Administrator'),(2, 'Manager'),(3, 'Front Of House'), (4, 'Back of House'),(5,'Busser');
+delete from user_role;
+-- delete from Role;
+delete from user;
+-- insert into Role values (1, 'Administrator'),(2, 'Manager'),(3, 'Front Of House'), (4, 'Back of House'),(5,'Busser');
+insert into user values (1, 'System', 'Administrator', 'Admin', 'password', '1988/05/08');
+insert into user_role values (1, 'System Administrator', 1);
