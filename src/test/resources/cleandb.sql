65d0af2561258c3ec53b7e820364032c702aa4b9
Julie Cortez <jlcortez@madisoncollege.edu>
2019-10-06 07:59:55 -0500

    updated to reflect changes to the tables

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,4 +1,4 @@
 delete from Role;
 delete from user;
 insert into user values (1, 'System', 'Administrator', 'Admin', 'password', '1988/05/08');
-insert into role values (1, 'System Administrator', 'Admin');
+insert into role values (1, 'Administrator', 1, 'Admin');
