0476ca462918025192c032e7700e370a8629307f
Julie Cortez <jlcortez@madisoncollege.edu>
2019-10-09 11:27:49 -0500

    add additional user and role record insert statements

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -2,3 +2,5 @@ delete from Role;
 delete from user;
 insert into user values (1, 'System', 'Administrator', 'Admin', 'password', '1988/05/08');
 insert into role values (1, 'Administrator', 1, 'Admin');
+insert into user values (2, 'Tammy', 'Johnson', 'TJohnson', 'password', '1992/10/02');
+insert into role values (2, 'Manager', 2, 'TJohnson');
