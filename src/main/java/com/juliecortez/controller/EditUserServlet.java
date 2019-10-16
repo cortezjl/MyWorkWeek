e1c9d34f8988ef56c6a52923efaf678bd20c2736
Julie Cortez <jlcortez@madisoncollege.edu>
2019-10-16 15:18:19 -0500

    added password, start and end date fields

--- src/main/java/com/juliecortez/controller/EditUserServlet.java
+++ src/main/java/com/juliecortez/controller/EditUserServlet.java
@@ -1,7 +1,10 @@
 package com.juliecortez.controller;
 
+import com.juliecortez.entity.Role;
 import com.juliecortez.entity.User;
 import com.juliecortez.persistence.GenericDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,11 +16,15 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Edit the user
  *
  * @author JCortez
+ * sources used: https://www.javaguides.net/2019/03/jsp-servlet-jdbc-mysql-crud-example-tutorial.html
  */
 @WebServlet(
         urlPatterns = {"/editUserServlet"}
@@ -26,39 +33,78 @@ import java.time.format.DateTimeFormatter;
 public class EditUserServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         GenericDao userDao = new GenericDao(User.class);
+        String[] ids = request.getParameterValues("roleId");
+        String[] roleValues = request.getParameterValues("roleName");
+        int loopCounter = 0;
+        for (String id : ids) {
+            String updatedRole = roleValues[loopCounter];
+            updateRole(id, updatedRole);
+            loopCounter = loopCounter + 1;
+        }
 
-        // Instantiate User Object
-        User user = new User();
-        // Set User values from the EditUser Form
-        user.setId(Integer.valueOf(req.getParameter("id")));
-        user.setFirstName(req.getParameter("firstName"));
-        user.setLastName(req.getParameter("lastName"));
-        user.setUserName(req.getParameter("userName"));
-        user.setDateOfBirth(LocalDate.parse((req.getParameter("dateOfBirth")), dateTimeFormatter));
-        //user.setRoles(req.getParameter("roles"));
+        User user = (User)userDao.getById(Integer.valueOf(request.getParameter("id")));
+        user.setFirstName(request.getParameter("firstName"));
+        user.setLastName(request.getParameter("lastName"));
+        user.setUserName(request.getParameter("userName"));
+        user.setPassword(request.getParameter("password"));
+        user.setDateOfBirth(LocalDate.parse((request.getParameter("dateOfBirth")), dateTimeFormatter));
+        user.setStartDate(LocalDate.parse((request.getParameter("startDate")), dateTimeFormatter));
+        user.setEndDate(LocalDate.parse((request.getParameter("endDate")), dateTimeFormatter));
 
-        // TODO Update roles values
+
+
         userDao.saveOrUpdate(user);
-        req.setAttribute("user", user);
+        request.setAttribute("user", user);
 
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/userEdit.jsp");
-        dispatcher.forward(req, resp);
+        // Access the session
+        HttpSession session = request.getSession();
+        // Add a message from updating the User to the session.
+        request.setAttribute("userUpdateMessage", "User has been updated");
+
+
+        // Create list of Roles to display as select list
+        List<String> roleList = new ArrayList<String>();
+        roleList.add(" ");
+        roleList.add("Administrator");
+        roleList.add("Back Of House");
+        roleList.add("Busser");
+        roleList.add("Front Of House");
+        roleList.add("Manager");
+        request.setAttribute("roleList",roleList);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/userEdit.jsp");
+        dispatcher.forward(request, response);
 
     }
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         GenericDao userDao = new GenericDao(User.class);
-        req.setAttribute("user", (User)userDao.getById(Integer.valueOf(req.getParameter("id"))));
+        request.setAttribute("user",(User)userDao.getById(Integer.valueOf(request.getParameter("id"))));
 
-        // Access the session
-        HttpSession session = req.getSession();
-        // Add a message from updating the User to the session.
-        session.setAttribute("userUpdateMessage", "User has been updated");
+        // Create list of Roles to display as select list
+        List<String> roleList = new ArrayList<String>();
+        roleList.add(" ");
+        roleList.add("Administrator");
+        roleList.add("Back Of House");
+        roleList.add("Busser");
+        roleList.add("Front Of House");
+        roleList.add("Manager");
+        request.setAttribute("roleList",roleList);
 
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/userEdit.jsp");
-        dispatcher.forward(req, resp);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/userEdit.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void  updateRole(String roleId, String newRole) {
+        GenericDao roleDao = new GenericDao(Role.class);
+        // Retrieve the Role record to update
+        Role roleToUpdate = (Role)roleDao.getById(Integer.parseInt(roleId));
+        // Set the updated role value
+        roleToUpdate.setRole(newRole);
+        // Update the record
+        roleDao.saveOrUpdate(roleToUpdate);
     }
 }
