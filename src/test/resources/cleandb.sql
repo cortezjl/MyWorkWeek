7e131915a95a9e696a0e83fc089167678cffbcfd
Julie Cortez <jlcortez@madisoncollege.edu>
2019-10-16 15:14:07 -0500

    update to add password, start and end dates for user

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,6 +1,7 @@
 delete from Role;
 delete from user;
-insert into user values (1, 'System', 'Administrator', 'Admin', 'password', '1988/05/08');
+insert into user values (1, 'System', 'Administrator', 'Admin', 'password', '2000/01/01', '2019/01/01', '9999/12/31');
 insert into role values (1, 'Administrator', 1, 'Admin');
-insert into user values (2, 'Tammy', 'Johnson', 'TJohnson', 'password', '1992/10/02');
+insert into user values (2, 'Tammy', 'Johnson', 'TJohnson', 'password', '1992/10/02', '2017/10/18', '9999/12/31');
 insert into role values (2, 'Manager', 2, 'TJohnson');
+insert into role values (3, 'Administrator', 2, 'TJohnson');
