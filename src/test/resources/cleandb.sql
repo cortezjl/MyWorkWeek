2c44ecdf6b68e5076d5d2b04bed16ce46b84888d
Julie Cortez <jlcortez@madisoncollege.edu>
2019-11-30 16:43:53 -0600

    chnaged start and end date to LocalDateTime so updated insert of record

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -6,4 +6,4 @@ insert into role values (1, 'Administrator', 1, 'admin');
 insert into user values (2, 'Tammy', 'Johnson', 'TJohnson', 'password', '1992/10/02', '2017/10/18', '9999/12/31');
 insert into role values (2, 'Manager', 2, 'TJohnson');
 insert into role values (3, 'Administrator', 2, 'TJohnson');
-insert into time_off_request values (1, 1, 'admin', '2019/11/15', '2019/11/18', '12:00 AM', '12:00 PM');
+insert into time_off_request values (1, 1, 'admin', '2019/11/15 08:00:00', '2019/11/15 15:00:00');
