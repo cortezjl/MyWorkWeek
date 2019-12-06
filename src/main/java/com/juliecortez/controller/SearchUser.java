2af4f1827215f3c51cf32adef580e518fb1ca458
Julie Cortez <jlcortez@madisoncollege.edu>
2019-12-05 18:03:09 -0600

    changed to add comments, change system out to logger and comment it out

--- src/main/java/com/juliecortez/controller/SearchUser.java
+++ src/main/java/com/juliecortez/controller/SearchUser.java
@@ -4,6 +4,8 @@ import com.juliecortez.entity.User;
 import com.juliecortez.persistence.GenericDao;
 import com.juliecortez.persistence.SessionFactoryProvider;
 import com.juliecortez.persistence.UserDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
@@ -20,7 +22,7 @@ import java.util.HashSet;
 import java.util.List;
 
 /**
- * A servlet used to search for users.
+ * A servlet used to search for users based on the type of search the user chose.
  * @author JCortez
  */
 
@@ -29,26 +31,38 @@ import java.util.List;
 )
 
 public class SearchUser extends HttpServlet {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         GenericDao userDao = new GenericDao(User.class);
-        // get parameters from request
+        // get parameters from the request
         String searchType = req.getParameter("searchType");
         String searchField = req.getParameter("searchField");
         String searchValue = req.getParameter("searchValue");
+        // retrieve list of qualifying users based on the type of search requested
         if (searchType.equals("allUsers")) {
+            // retrieve all users
             req.setAttribute("users", userDao.getAll());
         } else if (searchType.equals("activeUsers")) {
+            // retrieve all users that are active as of today
             req.setAttribute("users", getUsersActiveToday());
         } else {
+            // retrieve all users that qualify against the search field and value chosen by the user
             req.setAttribute("users", userDao.getByPropertyLike(searchField, searchValue));
         }
+        // forward the request to the jsp that will display the qualifying user results
         RequestDispatcher dispatcher = req.getRequestDispatcher("userSearchResults.jsp");
         dispatcher.forward(req, resp);
     }
 
 
+    /**
+     * Gets a list of users that are active as of today.
+     *
+     * @return the users active today
+     */
     public List<User> getUsersActiveToday() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         Session session = SessionFactoryProvider.getSessionFactory().openSession();
@@ -60,7 +74,7 @@ public class SearchUser extends HttpServlet {
         query.setParameter("endDate",today);
         List<User> users = query.list();
 
-        System.out.println("size of user list returned from hibernate query is: " + query.list().size());
+        // logger.info("size of user list returned from hibernate query is: " + query.list().size());
 
         session.close();
         return users;
