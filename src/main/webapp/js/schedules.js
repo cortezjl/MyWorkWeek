357f8fd6ff98d4ae44a092f51c04e3fb55d2085e
Julie Cortez <jlcortez@madisoncollege.edu>
2019-12-06 18:15:27 -0600

    add new file to use for any javascript that might be needed for form

---
+++ src/main/webapp/js/schedules.js
@@ -0,0 +1,19 @@
+/*------------------------------------------------------
+This is the JS library file for schedules.jsp page
+-------------------------------------------------------*/
+/*
+   The purpose of this function is to edit data on the
+   schedules page, which is used to start a find or add for schedules
+   Author:        Julie Cortez
+   Date Created:  12/06/2019
+*/
+
+
+/*
+   The purpose of this function is to clear any styles applied for validation errors
+*/
+function clearForm() {
+    // reset background color on fields being edited
+    document.schedulesForm.startDate.style = "";
+}  // end of clearForm function
+
