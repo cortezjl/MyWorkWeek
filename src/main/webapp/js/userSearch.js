bc2ca9ad6c87953caa94c40088d3c6af752c72bb
Julie Cortez <jlcortez@madisoncollege.edu>
2019-12-06 18:16:31 -0600

    add new file to use for any javascript that might be needed for form

---
+++ src/main/webapp/js/userSearch.js
@@ -0,0 +1,19 @@
+/*------------------------------------------------------
+This is the JS library file for userSearch.jsp page
+-------------------------------------------------------*/
+/*
+   The purpose of this function is to edit data on the
+   user search page
+   Author:        Julie Cortez
+   Date Created:  12/06/2019
+*/
+
+
+/*
+   The purpose of this function is to clear any styles applied for validation errors
+*/
+function clearForm() {
+    // reset background color on fields being edited
+    document.userSearchForm.searchValue.style = "";
+}  // end of clearForm function
+
