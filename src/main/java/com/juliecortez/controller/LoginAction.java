b83f0e96cd596c91d3a7d754ed5e71f258f101fc
Julie Cortez <jlcortez@madisoncollege.edu>
2019-12-08 08:15:37 -0600

    played with putting in call for weather api but took it out

--- src/main/java/com/juliecortez/controller/LoginAction.java
+++ src/main/java/com/juliecortez/controller/LoginAction.java
@@ -1,8 +1,11 @@
 package com.juliecortez.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+    import com.fasterxml.jackson.databind.ObjectMapper;
     import org.apache.logging.log4j.LogManager;
     import org.apache.logging.log4j.Logger;
+    import org.openweathermap.WeatherResponse;
 
     import javax.servlet.RequestDispatcher;
     import javax.servlet.ServletException;
@@ -10,6 +13,11 @@ package com.juliecortez.controller;
     import javax.servlet.http.HttpServlet;
     import javax.servlet.http.HttpServletRequest;
     import javax.servlet.http.HttpServletResponse;
+    import javax.servlet.http.HttpSession;
+    import javax.ws.rs.client.Client;
+    import javax.ws.rs.client.ClientBuilder;
+    import javax.ws.rs.client.WebTarget;
+    import javax.ws.rs.core.MediaType;
     import java.io.IOException;
 
 
@@ -27,10 +35,11 @@ public class LoginAction extends HttpServlet {
     private final Logger logger = LogManager.getLogger(this.getClass());
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        logger.info("The logged in user: " + req.getRemoteUser() + " has a role of Administrator: " + req.isUserInRole("Administrator"));
-        logger.info("The logged in user: " + req.getRemoteUser() + " has a role of Manager: " + req.isUserInRole("Manager"));
+        //logger.info("The logged in user: " + req.getRemoteUser() + " has a role of Administrator: " + req.isUserInRole("Administrator"));
+        //logger.info("The logged in user: " + req.getRemoteUser() + " has a role of Manager: " + req.isUserInRole("Manager"));
         // Forward the request back to the home page
         RequestDispatcher dispatcher = req.getRequestDispatcher("/index.jsp");
         dispatcher.forward(req, resp);
     }
+
 }
