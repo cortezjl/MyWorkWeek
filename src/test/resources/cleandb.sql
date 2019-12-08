f2c6b4dc45a34a14ae998b84fd9ff91d22e27b9e
Julie Cortez <jlcortez@madisoncollege.edu>
2019-12-08 08:09:45 -0600

    correct end date value for schedule record being inserted

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -9,5 +9,5 @@ insert into user values (2, 'Tammy', 'Johnson', 'TJohnson', 'password', '1992/10
 insert into role values (2, 'Manager', 2, 'TJohnson');
 insert into role values (3, 'Administrator', 2, 'TJohnson');
 insert into time_off_request values (1, 1, 'admin', '2019/11/15 08:00:00', '2019/11/15 15:00:00');
-insert into schedule values (1, '2019/11/11', '2019/11/18');
+insert into schedule values (1, '2019/11/11', '2019/11/17');
 insert into schedule_detail values (1, 'open', '04:00 PM', 'open', '04:00 PM','open', '04:00 PM','open', '04:00 PM','open', '04:00 PM','open', '02:00 PM','open', '02:00 PM', 'admin', 1, 1);
